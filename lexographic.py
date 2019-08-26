26f976ab998b7bfecff7a6f825f3ad5e64f73074
Edvard Pedersen <edvard.pedersen@uit.no>
2019-08-26 14:50:04 +0200

    simplify lexographic solution

--- lexographic.py
+++ lexographic.py
@@ -2,22 +2,27 @@ from collections import defaultdict
 from itertools import combinations
 from solution import Solution
 
-def check_legal(new, current):
-    for elem in current:
-        if new[0] in elem:
-            if new[1] in elem or new[2] in elem:
-                return False
-        if new[1] in elem and new[2] in elem:
-            return False
-    return True
-
-
 def lexographic(number, selections):
+    # Generate a list of all participants
     students = range(1,number+1)
+
+    # Generate all combinations (k choose 3)
     combs = list(combinations(students, selections))
+
+    # Always start with the lexographically first combination
     current = [combs[0]]
+
+    # Iterate over all combinations after first
     for el in combs[1:]:
-        if check_legal(el, current):
+        # Verify that this combination is legal to select
+        new = set(el)
+        legal = True
+        for elem in current:
+            if len(new.intersection(elem)) >= 2:
+                legal = False
+
+        # Only add this to the list, if it is actually legal to add
+        if legal:
             current.append(el)
 
     return [Solution(current, students)]
